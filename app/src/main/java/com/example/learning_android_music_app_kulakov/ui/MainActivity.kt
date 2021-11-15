e53aaf2cd73c7168af2eeac4c6d1f6ed02ba003e
Alexander <a.kulakov.vironit@vironit.ru>
2021-11-15 10:57:25 +0300

    initial commit

---
+++ app/src/main/java/com/example/learning_android_music_app_kulakov/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.learning_android_music_app_kulakov.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.learning_android_music_app_kulakov.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
