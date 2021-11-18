85f57cee67347b3836b292b42542de48b1663531
Alexander <a.kulakov.vironit@vironit.ru>
2021-11-18 10:11:04 +0300

    fix: add dependency in viewModelModule

--- app/src/main/java/com/example/learning_android_music_app_kulakov/di/ViewModelModule.kt
+++ app/src/main/java/com/example/learning_android_music_app_kulakov/di/ViewModelModule.kt
@@ -2,12 +2,13 @@ package com.example.learning_android_music_app_kulakov.di
 
 import com.example.learning_android_music_app_kulakov.ui.fragments.main.MainVM
 import com.example.learning_android_music_app_kulakov.ui.fragments.song.SongVM
+import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel {
-        MainVM(get())
+        MainVM(androidApplication(), get())
     }
 
     viewModel {
